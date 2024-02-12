cf112ba5278c31b3cfff61cc49aa20afde95bab0
andrewmatheus <andrewmatheus@gmail.com>
2024-02-12 16:45:43 -0300

    fix: finish first exercise

--- M1S05EX01/src/Main.java
+++ M1S05EX01/src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello");
+        Player player1 = new Player("Andrew", 33);
+        player1.addPoints(10);
+        player1.addAttempt();
+        player1.displayInformation();
+
+        Player player2 = new Player("Miguel", 10);
+        player2.addPoints(10);
+        player2.addAttempt();
+        player2.displayInformation();
+
     }
 }
--- M1S05EX01/src/Player.java
+++ M1S05EX01/src/Player.java
@@ -1,23 +1,23 @@
 public class Player {
-    /*
-     * Attributes
-     * */
+    // <editor-fold desc="Attributes">
     private String name;
     private int age;
     private double punctuation;
     private int numberAttempts;
+    // </editor-fold>
 
-    /*
-     * Constructor
-     * */
-
+    // <editor-fold desc="Constructor">
     public Player(String name, int age) {
         this.name = name;
         this.age = age;
         this.punctuation = 0;
         this.numberAttempts = 0;
     }
+    // </editor-fold>
 
+    // <editor-fold desc="methods">
+
+    // <editor-fold desc="methods-attributes">
     public String getName() {
         return name;
     }
@@ -49,4 +49,34 @@ public class Player {
     public void setNumberAttempts(int numberAttempts) {
         this.numberAttempts = numberAttempts;
     }
+    // </editor-fold>
+
+    // <editor-fold desc="methods-player">
+
+    public void addPoints(int points) {
+        this.punctuation += points;
+    }
+
+    public void losePoints(int points) {
+        this.punctuation -= points;
+
+        if (this.punctuation < 0) {
+            this.punctuation = 0;
+        }
+    }
+
+    public void addAttempt() {
+        this.numberAttempts++;
+    }
+
+    public void displayInformation() {
+        System.out.println("Nome: " + this.name);
+        System.out.println("Idade: " + this.age);
+        System.out.println("Pontuação: " + this.punctuation);
+        System.out.println("Número de Tentativas: " + this.numberAttempts);
+    }
+
+    // </editor-fold>
+
+    // </editor-fold>
 }
