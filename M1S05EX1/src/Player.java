30cb663556be88b5b6de2706e246a08051a363d0
andrewmatheus <andrewmatheus@gmail.com>
2024-02-12 15:44:52 -0300

    feat: first commit Intellij and exercises of the week 5

---
+++ M1S05EX1/src/Player.java
@@ -0,0 +1,52 @@
+public class Player {
+    /*
+    * Attributes
+    * */
+    private String name;
+    private int age;
+    private double punctuation;
+    private int numberAttempts;
+
+    /*
+     * Constructor
+     * */
+
+    public Player(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.punctuation = 0;
+        this.numberAttempts = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getPunctuation() {
+        return punctuation;
+    }
+
+    public void setPunctuation(double punctuation) {
+        this.punctuation = punctuation;
+    }
+
+    public int getNumberAttempts() {
+        return numberAttempts;
+    }
+
+    public void setNumberAttempts(int numberAttempts) {
+        this.numberAttempts = numberAttempts;
+    }
+}
